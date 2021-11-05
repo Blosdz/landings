3b4d849c6fffe40aadab6601ab1b99c0bcd7367c
Dante Cuevas <danieldantecuevas@gmail.com>
2021-11-05 11:38:36 -0500

    add template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/welcome_default', function () {
+    return view('welcome_default');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
