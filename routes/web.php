a513db97900ee8b9096eacc5c8aca8b0726edf00
javier <edgarjquispe97@gmail.com>
2022-02-22 12:31:59 -0500

    adding 'get and delete' routes

--- routes/web.php
+++ routes/web.php
@@ -66,3 +66,5 @@ Route::resource('notifications', App\Http\Controllers\NotificationController::cl
 
 
 Route::resource('events', App\Http\Controllers\EventController::class);
+Route::get('/rejection-history/{user_id}',[App\Http\Controllers\EventController::class,'rejectionHistory'])->name('rejectionHistory');
+Route::delete('/rejection-history/{user_id}',[App\Http\Controllers\EventController::class,'DeleteRejectionHistory'])->name('DeleteRejectionHistory');
