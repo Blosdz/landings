c09045f04870fe2dffb5e1e758e8bad572041fdc
javier <edgarjquispe97@gmail.com>
2022-02-23 11:54:43 -0500

    removed route 'delete'

--- routes/web.php
+++ routes/web.php
@@ -66,5 +66,4 @@ Route::resource('notifications', App\Http\Controllers\NotificationController::cl
 
 
 Route::resource('events', App\Http\Controllers\EventController::class);
-Route::get('/rejection-history/{user_id}',[App\Http\Controllers\EventController::class,'rejectionHistory'])->name('rejectionHistory');
-Route::delete('/rejection-history/{id}',[App\Http\Controllers\EventController::class,'DeleteRejectionHistory'])->name('DeleteRejectionHistory');
+Route::get('/rejection-history/{user_id}',[App\Http\Controllers\RejectionHistoryController::class,'rejectionHistory'])->name('rejectionHistory');
