b1cad27948d16ac2934ed23e1b4bb106cdd446e7
javier <edgarjquispe97@gmail.com>
2022-03-28 18:35:29 -0500

    field modification for admin panel resources/views/profiles/fields2_rol3_2.blade.php

--- resources/views/profiles/fields_2.blade.php
+++ resources/views/profiles/fields_2.blade.php
@@ -16,27 +16,39 @@
     {!! Form::text('lastname2', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Country Document Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('country_document', 'País emisor del documento de identidad:') !!}
-    {!! Form::text('country_document2', null, ['class' => 'form-control']) !!}
-</div>
-
 <!-- Type Document Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('type_document', 'Tipo de documento de identidad:') !!}
     {!! Form::text('type_document2', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Birthdate Field -->
+<!-- Number Document Field -->
 <div class="form-group col-sm-6">
+    {!! Form::label('number_document', 'Número de documento de indentidad:') !!}
+    {!! Form::text('identification_number2', null, ['class' => 'form-control',  'onkeypress'=>'return isNumber(event)']) !!}
+</div>
+
+<!-- Country Document Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('country_document', 'País emisor del documento de identidad:') !!}
+    {!! Form::text('country_document2', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Sex Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('sex', 'Sexo:') !!}
+    {!! Form::select('sex2', $sex_list, null, ['class' => 'form-control','empty'=>'Seleccionar']) !!}
+</div>
+
+<!-- Birthdate Field -->
+<div class="form-group col-sm-6" style="display: none;">
     {!! Form::label('birthdate', 'Fecha de nacimiento:') !!}
     {!! Form::text('birthdate2', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Nacionality Field -->
 
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6" style="display: none;">
     {!! Form::label('nacionality', 'Nacionalidad:') !!}
     {!! Form::text('nacionality2', null, ['class' => 'form-control']) !!}
 </div>
@@ -47,16 +59,16 @@
 
 <div class="form-inline">
 <!-- City Field -->
-    <div class="form-group col-sm-2">
-        {!! Form::label('country', 'Pais:') !!}
+    <div class="form-group col-sm-3">
+        {!! Form::label('country', 'País:') !!}
         {!! Form::text('country2', null, ['class' => 'form-control']) !!}
     </div>
 
-    <div class="form-group col-sm-2">
-        {!! Form::label('city', 'Region:') !!}
+    <div class="form-group col-sm-3">
+        {!! Form::label('city', 'Región:') !!}
         {!! Form::text('city2', null, ['class' => 'form-control']) !!}
     </div>
-    <div class="form-group col-sm-2">
+    <div class="form-group col-sm-3">
         {!! Form::label('state', 'Cuidad:') !!}
         {!! Form::text('state2', null, ['class' => 'form-control']) !!}
     </div>
@@ -64,14 +76,25 @@
 
 <div class="form-group col-sm-6">
     {!! Form::label('address', 'Dirección de recidencia:') !!}
-    {!! Form::text('nacionality2', null, ['class' => 'form-control']) !!}
+    {!! Form::text('address2', null, ['class' => 'form-control']) !!}
 </div>
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6" style="display: none;">
     {!! Form::label('phone', 'Número de celular:') !!}
     {!! Form::text('phone2', null, ['class' => 'form-control']) !!}
 </div>
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6" style="display: none;">
     {!! Form::label('job', 'Ocupación o profesión:') !!}
     {!! Form::text('job2', null, ['class' => 'form-control']) !!}
+</div>
+
+<div class="form-group col-sm-6">
+    {!! Form::label('photo', 'foto de perfil:') !!}
+    <br>
+    <img src="{{ url('/storage', $profile->profile_picture2) }}" width="300px"/>
+
+</div>
+<div class="form-group col-sm-6">
+    {!! Form::label('address_wallet', 'Dirección Wallet:') !!}
+    {!! Form::text('address_wallet2', null, ['class' => 'form-control']) !!}
 </div>
 
--- resources/views/profiles/fields_3.blade.php
+++ resources/views/profiles/fields_3.blade.php
@@ -16,27 +16,39 @@
     {!! Form::text('lastname3', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Country Document Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('country_document', 'País emisor del documento de identidad:') !!}
-    {!! Form::text('country_document3', null, ['class' => 'form-control']) !!}
-</div>
-
 <!-- Type Document Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('type_document', 'Tipo de documento de identidad:') !!}
     {!! Form::text('type_document3', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Birthdate Field -->
+<!-- Number Document Field -->
 <div class="form-group col-sm-6">
+    {!! Form::label('number_document', 'Número de documento de indentidad:') !!}
+    {!! Form::text('identification_number3', null, ['class' => 'form-control',  'onkeypress'=>'return isNumber(event)']) !!}
+</div>
+
+<!-- Country Document Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('country_document', 'País emisor del documento de identidad:') !!}
+    {!! Form::text('country_document3', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Sex Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('sex', 'Sexo:') !!}
+    {!! Form::select('sex3', $sex_list, null, ['class' => 'form-control','empty'=>'Seleccionar']) !!}
+</div>
+
+<!-- Birthdate Field -->
+<div class="form-group col-sm-6" style="display: none;">
     {!! Form::label('birthdate', 'Fecha de nacimiento:') !!}
     {!! Form::text('birthdate3', null, ['class' => 'form-control']) !!}
 </div>
 
 <!-- Nacionality Field -->
 
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6" style="display: none;">
     {!! Form::label('nacionality', 'Nacionalidad:') !!}
     {!! Form::text('nacionality3', null, ['class' => 'form-control']) !!}
 </div>
@@ -47,16 +59,16 @@
 
 <div class="form-inline">
 <!-- City Field -->
-    <div class="form-group col-sm-2">
-        {!! Form::label('country', 'Pais:') !!}
+    <div class="form-group col-sm-3">
+        {!! Form::label('country', 'País:') !!}
         {!! Form::text('country3', null, ['class' => 'form-control']) !!}
     </div>
 
-    <div class="form-group col-sm-2">
-        {!! Form::label('city', 'Region:') !!}
+    <div class="form-group col-sm-3">
+        {!! Form::label('city', 'Región:') !!}
         {!! Form::text('city3', null, ['class' => 'form-control']) !!}
     </div>
-    <div class="form-group col-sm-2">
+    <div class="form-group col-sm-3">
         {!! Form::label('state', 'Cuidad:') !!}
         {!! Form::text('state3', null, ['class' => 'form-control']) !!}
     </div>
@@ -64,14 +76,24 @@
 
 <div class="form-group col-sm-6">
     {!! Form::label('address', 'Dirección de recidencia:') !!}
-    {!! Form::text('nacionality3', null, ['class' => 'form-control']) !!}
+    {!! Form::text('address3', null, ['class' => 'form-control']) !!}
 </div>
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6" style="display: none;">
     {!! Form::label('phone', 'Número de celular:') !!}
     {!! Form::text('phone3', null, ['class' => 'form-control']) !!}
 </div>
-<div class="form-group col-sm-6">
+<div class="form-group col-sm-6" style="display: none;">
     {!! Form::label('job', 'Ocupación o profesión:') !!}
     {!! Form::text('job3', null, ['class' => 'form-control']) !!}
 </div>
 
+<div class="form-group col-sm-6">
+    {!! Form::label('photo', 'foto de perfil:') !!}
+    <br>
+    <img src="{{ url('/storage', $profile->profile_picture3) }}" width="300px"/>
+
+</div>
+<div class="form-group col-sm-6">
+    {!! Form::label('address_wallet', 'Dirección Wallet:') !!}
+    {!! Form::text('address_wallet3', null, ['class' => 'form-control']) !!}
+</div>
