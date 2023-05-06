a6c7bf84626db2991a7f6f69250ef541606f84e8
Diego Toledo <diego.toledo@apselom.com>
2023-05-06 06:44:02 -0500

    fix: schedule tasks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdatePaymentStatus;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        UpdatePaymentStatus::class,
     ];
 
     /**
@@ -38,5 +39,6 @@ class Kernel extends ConsoleKernel
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
+
     }
 }
