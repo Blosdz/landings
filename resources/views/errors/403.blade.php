f4873a41704d8c9673798ce5c6bc81e6746e0b0c
Diego Toledo <diego.toledo@apselom.com>
2023-05-15 12:20:49 -0500

    feat: block user when the user hasn't paid

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+  <div class="container-fluid">
+        <div class="animated fadeIn">
+             <div class="row">
+                 <div class="col-lg-12">
+                     <div class="card">
+                        <div class="card-body text-center">
+                            <img src="/images/bot-icon.png" alt="User profile" class="img-fluid" style="height: 50vh">
+                            <h1>No autorizado</h1>
+                        </div>
+                     </div>
+                 </div>
+             </div>
+        </div>
+  </div>
+@endsection
